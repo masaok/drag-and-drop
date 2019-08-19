9344bf5278b2e72f2e6aa4c34c841b63f569919f
Masao Kitamura <masaok@gmail.com>
2019-08-19 00:44:57 -0700

    cleaning up the code; preparing to convert to tables and rows

--- bother7-table/src/components/CardContainer.js
+++ bother7-table/src/components/CardContainer.js
@@ -3,7 +3,16 @@ import Card from './Card';
 
 const CardContainer = (props) => {
   return (<div className="wrapper" onDragOver={props.drag}>
-  {props.info.map((blurb, index) => {return <Card title={blurb.title} swap={props.swap} index={index+1} details={blurb.details}/>})}
+  {props.info.map((blurb, index) => {
+    return (
+      <Card 
+        title={blurb.title} 
+        swap={props.swap} 
+        index={index+1} 
+        details={blurb.details}/>
+      )
+    })
+  }
   </div>)
 }
 
