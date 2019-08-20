444f69c13edb11f8c21977649d746d7f9679b69a
Masao Kitamura <masaok@gmail.com>
2019-08-20 00:19:17 -0700

    Pluralsight drag drop demo added

---
+++ pluralsight-react-dnd/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import ToDoDragDropDemo from './ToDoDragDropDemo'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ToDoDragDropDemo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
