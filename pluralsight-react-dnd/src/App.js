8fd4cbb4a263b6e7abf609d7dfaa75fa65c9fa72
Masao Kitamura <masaok@gmail.com>
2019-08-27 20:29:55 -0700

    preparing MUI in isolation

--- pluralsight-react-dnd/src/App.js
+++ pluralsight-react-dnd/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import ToDoDragDropDemo from './ToDoDragDropDemo'
+import ToDoDragDropDemoMUI from './ToDoDragDropDemoMUI'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <ToDoDragDropDemo />
+        <ToDoDragDropDemoMUI />
       </header>
     </div>
   );
